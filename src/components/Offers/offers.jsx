7fcf6b53c63ae8e2c83afbcada7d8e65e3718546
gopi-krishnan-k <gopikrishnan0008@gmail.com>
2025-09-23 22:43:42 +0530

    trip-webstite-initiate

---
+++ src/components/Offers/offers.jsx
@@ -0,0 +1,11 @@
+
+import React from "react";
+import './offers.css'
+
+const Offers = () =>{
+    return (
+        <div>offers</div>
+    )
+}
+
+export default Offers;
---
+++ src/components/Popular/popular.jsx
@@ -0,0 +1,11 @@
+
+import React from "react";
+import './popular.css'
+
+const Popular = () =>{
+    return (
+        <div>popular</div>
+    )
+}
+
+export default Popular;
